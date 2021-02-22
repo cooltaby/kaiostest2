54ee4c12ae84cfed48034af7a74063190ad06e8b
cooltaby <cooltaby@users.noreply.github.com>
2021-02-22 16:38:07 +0530

    Add files via upload

--- c2runtime.js
+++ c2runtime.js
@@ -27719,8 +27719,6 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Sprite.prototype.cnds.CompareFrame,
 	cr.plugins_.Browser.prototype.acts.Vibrate,
 	cr.behaviors.Bullet.prototype.acts.SetSpeed,
-	cr.plugins_.Keyboard.prototype.cnds.IsKeyCodeDown,
-	cr.plugins_.Keyboard.prototype.cnds.IsKeyDown,
 	cr.plugins_.Keyboard.prototype.cnds.OnKeyCode,
 	cr.plugins_.Sprite.prototype.cnds.IsVisible,
 	cr.plugins_.TiledBg.prototype.acts.SetVisible,
@@ -27745,5 +27743,6 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Browser.prototype.cnds.OnBackButton,
 	cr.plugins_.Browser.prototype.acts.Close,
 	cr.behaviors.Pin.prototype.acts.Pin,
-	cr.plugins_.Sprite.prototype.acts.SetPos
+	cr.plugins_.Sprite.prototype.acts.SetPos,
+	cr.plugins_.Keyboard.prototype.cnds.IsKeyCodeDown
 ];};
